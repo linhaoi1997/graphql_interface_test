2157bf8c91bd7fa226ba96c8e7a36939cf647658
林昊 <linhao@192.168.8.104>
2020-11-23 23:38:05 +0800

    该删的删了

--- support/data_maker/__init__.py
+++
@@ -1,6 +0,0 @@
-from .DataFaker import DataFaker
-from .DataMaker import DataMaker
-from .GraphqlClient import GraphqlClient
-from .InputSearcher import InputSearcher
-from .JsonSetter import JsonSetter
-from .VariableMaker import VariableMaker
