c58e3181c19ec0fa5e43044b3a4c77b95b88c3b5
lin <linhao@192.168.8.103>
2020-12-12 23:09:38 +0800

    test name

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     pro_dir = os.path.dirname(__file__)
     xml_path = pro_dir + "/output/report/xml/"
     # pytest.main(['-v', "-s", "-k", "delete", '--alluredir', xml_path])
-    # pytest.main(['-v', "-s", "-k", "not query and not delete", '--alluredir', xml_path])
+    pytest.main(['-v', "-s", "-k", "not query and not delete", '--alluredir', xml_path])
     # pytest.main(['-v', "-s", "-k", "employee", '--alluredir', xml_path])
     pytest.main(['-v', "-s", "test_cases", '--alluredir', xml_path])
     go_allure()
